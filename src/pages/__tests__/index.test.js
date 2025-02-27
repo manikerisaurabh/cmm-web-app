e96ae52d0711c5ef7eda1e53cac6647699607ed4
Parvez Khan <pk0106288@gmail.com>
2025-02-27 14:17:17 +0530

    Home Page

--- src/pages/__tests__/index.test.js
+++
@@ -1,8 +0,0 @@
-const { render, screen } = require('@testing-library/react');
-const Index = require('../index');
-
-test('renders index component', () => {
-	render(<Index />);
-	const linkElement = screen.getByText(/your expected text/i);
-	expect(linkElement).toBeInTheDocument();
-});
