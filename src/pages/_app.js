107559572815eaaf7d971466e97a813d43c3c46d
manikerisaurabh <manikerisaurabh@gmail.com>
2025-05-14 17:53:14 +0530

    updated as per SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,65 +4,64 @@ import Footer from "@/components/Footer";
 import Sidebar from "@/components/Sidebar";
 import { Container } from "@mui/material";
 import { SessionProvider } from "next-auth/react";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Poppins } from "next/font/google"; // Import Poppins from Next.js
 import SEO from "@/components/SEO";
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"], // Choose required weights
-  display: "swap",
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"], // Choose required weights
+    display: "swap",
 });
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#6C6AE2", // Your primary color
-    },
-    secondary: {
-      main: "#FF4081",
-    },
-  },
-  typography: {
-    fontFamily: "Poppins, sans-serif", // Apply Poppins to Material-UI
-    button: {
-      textTransform: "none", // Prevents uppercase text in buttons
-      fontWeight: "bold",
-    },
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: "50px", // Rounded corners
-          border:"1px solid #fff"
+    palette: {
+        primary: {
+            main: "#6C6AE2", // Your primary color
         },
-      },
+        secondary: {
+            main: "#FF4081",
+        },
     },
-  },
+    typography: {
+        fontFamily: "Poppins, sans-serif", // Apply Poppins to Material-UI
+        button: {
+            textTransform: "none", // Prevents uppercase text in buttons
+            fontWeight: "bold",
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: "50px", // Rounded corners
+                    border: "1px solid #fff",
+                },
+            },
+        },
+    },
 });
 
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <main className={poppins.className}>
-    <SessionProvider session={pageProps.session}>
-    <SEO
-        title="City Center Mall "
-        description="City Center Mall"
-        image="https://www.citycentermall.com/og-image.jpeg"
-        url="https://www.citycentermall.com/"
-      />
-      <ThemeProvider theme={theme}>
-      <Navbar />
-      {/* <Sidebar /> */}
-      {/* <Container > */}
-      <Component {...pageProps} />
-      {/* </Container> */}
-      <Footer />
-      </ThemeProvider>
-    </SessionProvider>
-    </main>
-  );
+    return (
+        <main className={poppins.className}>
+            <SessionProvider session={pageProps.session}>
+                <SEO
+                    title="City Center Mall "
+                    description="Shop top brands across electronics, fashion, home appliances, and more at City Center Mall — with fast 60-minute delivery, secure checkout, and unbeatable prices."
+                    image="https://www.citycentermall.com/og-image.jpeg"
+                    url="https://www.citycentermall.com/"
+                />
+                <ThemeProvider theme={theme}>
+                    <Navbar />
+                    {/* <Sidebar /> */}
+                    {/* <Container > */}
+                    <Component {...pageProps} />
+                    {/* </Container> */}
+                    <Footer />
+                </ThemeProvider>
+            </SessionProvider>
+        </main>
+    );
 }
 
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Container, Grid, Card, CardContent, Typography, Button, Box } from "@mui/material";
+import {
+    Container,
+    Grid,
+    Card,
+    CardContent,
+    Typography,
+    Button,
+    Box,
+} from "@mui/material";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -12,57 +20,68 @@ import SocialMedia from "@/components/Home/SocialMedia";
 import Hero from "@/components/Home/Hero";
 import Image from "next/image";
 import { Height } from "@mui/icons-material";
+import FAQSection from "@/components/Home/FAQSecction";
 
 // Home Page with Slider and Sections
 export default function HomePage() {
-  const slides = [
-    { title: "Fast & Reliable Delivery", description: "Get your orders delivered in no time!" },
-    { title: "Wide Range of Products", description: "Shop from multiple vendors at City Center Mall." },
-    { title: "Exclusive Deals & Discounts", description: "Save more with special offers!" }
-  ];
+    const slides = [
+        {
+            title: "Fast & Reliable Delivery",
+            description: "Get your orders delivered in no time!",
+        },
+        {
+            title: "Wide Range of Products",
+            description: "Shop from multiple vendors at City Center Mall.",
+        },
+        {
+            title: "Exclusive Deals & Discounts",
+            description: "Save more with special offers!",
+        },
+    ];
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  };
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    };
 
-  return (
-    <>
-      <Hero />
-      <Welcome />
-      <Box sx={{ position: "relative" }}>
-        <Image
-          src="/images/welcome/roundbg.png" // Replace with your image URL
-          alt="Hero Background"
-          layout="fill"
-          objectFit="cover"
-          className="secondbg"
-          sx={{
-            position: "absolute",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Categories />
-        <HowToShop />
-        <Delivery />
-      </Box>
-      <Box sx={{ position: "relative" }}>
-        <Image
-          src="/images/welcome/roundbglast.png" // Replace with your image URL
-          alt="Hero Background 2"
-          layout="fill"
-          objectFit="contain"
-          className="thirdbg"
-        />
-      <Testimonials />
-      <SocialMedia />
-      </Box>
-    </>
-  );
+    return (
+        <>
+            <Hero />
+            <Welcome />
+            <Box sx={{ position: "relative" }}>
+                <Image
+                    src="/images/welcome/roundbg.png" // Replace with your image URL
+                    alt="Hero Background"
+                    layout="fill"
+                    objectFit="cover"
+                    className="secondbg"
+                    sx={{
+                        position: "absolute",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                />
+                <Categories />
+                <HowToShop />
+                <Delivery />
+            </Box>
+            <Box sx={{ position: "relative" }}>
+                <Image
+                    src="/images/welcome/roundbglast.png" // Replace with your image URL
+                    alt="Hero Background 2"
+                    layout="fill"
+                    objectFit="contain"
+                    className="thirdbg"
+                />
+                <Testimonials />
+                <SocialMedia />
+                <FAQSection />
+            </Box>
+        </>
+    );
 }
